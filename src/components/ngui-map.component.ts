74c071edd0637229f23fb25f6dda0108870b5c2d
Sudarshan Chippa <sudarshan.chippa@qvantel.com>
2017-12-20 11:34:51 +0530

    initial commit

--- src/components/ngui-map.component.ts
+++ src/components/ngui-map.component.ts
@@ -134,6 +134,7 @@ export class NguiMapComponent implements OnChanges, OnDestroy, AfterViewInit, Af
           });
         }
       });
+      
 
       // update map when input changes
       debounceTime.call(this.inputChanges$, 1000)
